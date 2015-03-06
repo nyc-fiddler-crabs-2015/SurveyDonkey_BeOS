ba7ffa831b7c2dba7fb159460f5b47de0f04943a
Kiran <phanendar@gmail.com>
2015-03-06 14:41:44 -0500

    make necessary changes to the skel for the project

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,15 +1,3 @@
 get '/' do
   erb :index
 end
-
-get '/.json' do
-  if params[:code] == "1234"
-  {secret_user: 'East Philosophy'}.to_json
-else
-  403
-  end
-end
-
-get '/guess' do
-  erb :guess
-end
--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-require 'faker'
-
-50.times do
-
-User.create(username: Faker::Name.first_name, password: "123", password_confirmation: "123", avatar: Faker::Avatar.image("my-own-slug"))
-end
