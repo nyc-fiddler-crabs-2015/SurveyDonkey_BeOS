c32f34248f95ebff9f1d5191485c87445a894b26
Kevin Alwell <kevalwell@gmail.com>
2015-03-06 18:10:06 -0500

    Create new survey form

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -2,7 +2,9 @@ get '/surveys/new' do
   erb :'/surveys/new'
 end
 
-post '/surveys' do
+post '/surveys/new' do
+  #LINE 7 MUST BE MADE INTO AN ACTIVE COOKIE
+  params[:user_id] = 1
   @survey = Survey.create(params)
   redirect "/surveys/#{@survey.id}/edit"
 end
