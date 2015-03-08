9105baa25c98c282d0ba6ed2d209df9a5f48ed80
bruce wayne <no1_ldc@hotmail.com>
2015-03-08 13:11:50 -0400

    user setup

--- spec/controllers/auth_spec.rb
+++ spec/controllers/auth_spec.rb
@@ -2,7 +2,6 @@ require_relative "../spec_helper.rb"
 
 
 describe "Authentication controller"  do
-  let(:user) {User.create(first_name: "Demo", last_name: "Person", email: "demoperson@email.com", password: "password")}
-
+  let(:user) {User.create(email: "dongchul@email.com", password: "password")}
 
 end
