ad3cae07c5b2ffeb40437a34f4ca05b1892f98f9
bruce wayne <no1_ldc@hotmail.com>
2015-03-08 13:24:08 -0400

    add get/auth/signin test

--- spec/controllers/auth_spec.rb
+++ spec/controllers/auth_spec.rb
@@ -1,15 +1,23 @@
-require_relative "../spec_helper.rb"
+require_relative '../spec_helper.rb'
 
 
-describe "Authentication controller"  do
+describe 'Authentication controller'  do
   let(:user) {User.create(email: "dongchul@email.com", password: "password")}
 
-describe "GET /authentication/signup" do
-  it 'should load the sugnup page' do
+describe 'GET /authentication/signup' do
+  it 'should load the sign up page' do
     get '/authentication/signup'
     expect(last_response).to be_ok
   end
 end
+
+describe 'GET /authentication/signin' do
+  it 'should load the sign in page' do
+    get '/authentication/signin'
+    expect(last_response).to be_ok
+  end
+end
+
 
 
 
